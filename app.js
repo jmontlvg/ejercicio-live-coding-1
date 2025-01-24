1cef05ff3f697f48ba1042074b7ff9751c891a2e
Jackeline Montalvo <jackeline.montalvog@gmail.com>
2025-01-24 12:39:53 -0600

    Se agrega primer avance

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // Escribe tu código aquí.
+
+
+
+
+function funcionParametros(parametro1,  parametro2) {
+    let cadena = [];
+    const arreglo = ['gato', 'perro', 'ballena'];
+    arreglo.push()
+}
